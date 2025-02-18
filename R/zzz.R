437fa6ebd490785e89fa5fdd1bf9d7c812b7a8fa
Broccolito <wanjungu001@gmail.com>
2025-02-17 17:24:25 -0800

    automatic commit

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,24 @@
-.onLoad <- function(libname, pkgname) {
-  if (!requireNamespace("metaCCA", quietly = TRUE)) {
+.onLoad = function(libname, pkgname){
+  if (!requireNamespace("BiocManager", quietly = TRUE)){
     message("Installing metaCCA from Bioconductor...")
     BiocManager::install("metaCCA", ask = FALSE)
+  }
+  if (!requireNamespace("metaCCA", quietly = TRUE)){
+    message("Installing metaCCA from Bioconductor...")
+    BiocManager::install("metaCCA", ask = FALSE)
+  }
+  if (!requireNamespace("pleiosim", quietly = TRUE)){
     message("Installing pleiosim from GitHub")
     devtools::install_github("Broccolito/pleiosim")
+  }
+  if (!requireNamespace("MPAT", quietly = TRUE)){
     message("Installing MPAT from GitHub")
     devtools::install_github("Broccolito/MPATclone")
+  }
+  if (!requireNamespace("MTARclone", quietly = TRUE)){
     message("Installing MTAR from GitHub")
     devtools::install_github("Broccolito/MTARclone")
   }
+
 }
 
